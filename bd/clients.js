f3c51122cd080617dfda46dfa9e4a07e7c3587c9
Henrique <rique.duarte@outlook.com.br>
2021-09-28 12:49:11 -0300

    ajusting clients;

--- bd/clients.js
+++ bd/clients.js
@@ -2,10 +2,11 @@ const Sequelize = require('sequelize')
 const sequelize = require('./conect')
 
 const clients = sequelize.define('clients', {
-    name: {type: Sequelize.STRING, notNull: true},
+    name: {type: Sequelize.STRING, notNull: true, primaryKey: true },
     age: {type: Sequelize.INTEGER, notNull: true},
     tel: {type: Sequelize.STRING, notNull: true},
     birthday: {type: Sequelize.STRING, notNull: true},
+    social: {type: Sequelize.STRING},
   })
 
   clients.sync({})
--- bd/schedule.js
+++ bd/schedule.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const sequelize = require('./conect')
 
 const agenda = sequelize.define('agenda', {
-    cliente: {type: Sequelize.STRING, notNull: true},
+    cliente: {type: Sequelize.STRING, notNull: true, primaryKey: true },
     data: {type: Sequelize.STRING, notNull: true },
     horario: {type: Sequelize.STRING, notNull: true},
     procedimento: { type: Sequelize.STRING },
