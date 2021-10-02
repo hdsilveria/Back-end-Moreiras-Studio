8754bd81439488a533eb45216721957fc5d01bc0
Henrique Duarte <83184912+hdsilveria@users.noreply.github.com>
2021-10-02 17:04:21 -0300

    Changing acess to bd;

--- bd/conect.js
+++ bd/conect.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize("heroku_49ae44b7267e29e", "bb7dc15181b34c", "5572faf6", {
-    host: 'us-cdbr-east-04.cleardb.com',
+const sequelize = new Sequelize("studio_M", "usuario", "senha", {
+    host: 'localhost',
     dialect: 'mysql',
     PORT: 3306
   })
